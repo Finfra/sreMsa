1a89ee307332aa8a6c75744f5dbe32db544dab32
Steve J. South(NamJungGu) <nowage@gmail.com>
2025-05-18 12:50:44 +0000

    .

--- Lab1.Kubespray/Lab1.InstanceForTerraform/installOnEc2.sh
+++ Lab1.Kubespray/Lab1.InstanceForTerraform/installOnEc2.sh
@@ -1,53 +1,50 @@
 #!/bin/bash
 
 # Version Setting
-TERRAFORM_VERSION="1.8.3"
-ANSIBE_VERSION="9.5.1"           # core 2.16.6
+TERRAFORM_VERSION="1.12.0"
+ANSIBE_VERSION="11.5.0"              # core 2.18.5
 
 # System Variable Setting
 export LC_ALL=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
-echo "export LC_ALL=C.UTF-8">>/etc/bash.bashrc
-echo "export DEBIAN_FRONTEND=noninteractive">>/etc/bash.bashrc
+grep -qxF "export LC_ALL=C.UTF-8" /etc/bash.bashrc || echo "export LC_ALL=C.UTF-8" >> /etc/bash.bashrc
+grep -qxF "export DEBIAN_FRONTEND=noninteractive" /etc/bash.bashrc || echo "export DEBIAN_FRONTEND=noninteractive" >> /etc/bash.bashrc
 
-hostname i1
+hostname provisioner
 hostname > /etc/hostname
 
-
-
-apt -y update
-apt -y install docker.io docker-compose unzip mysql-client jq
-groupadd docker
-usermod -G docker ubuntu
-
+apt update
+apt -y install unzip curl jq git wget vim
+apt -y install locales
+locale-gen en_US.UTF-8
+update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
 # install pip
-apt install -y python3-full
-apt install -y python3-pip
-pip3 install numpy
-
-[[ -f /usr/bin/python ]]&&rm /usr/bin/python
-ln -s /usr/bin/python3.12 /usr/bin/python
-
-
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update
+apt install -y python3.11 # python3.11-venv python3.11-dev python3.11-distutils
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
+update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
 
 # install awscli
-python3 -m pip install awscli
+python -m pip install awscli
 #python3 -m pip install  awsebcli
 complete -C aws_completer aws
 
 # install ansible
-pip3 install netaddr jinja2
-pip3 install ansible==$ANSIBE_VERSION
+pip install netaddr jinja2
+pip install ansible==$ANSIBE_VERSION
 
 # for Language Setting
-cat <<EOF>> /etc/bash.bashrc
+grep -qxF "set input-meta on" /etc/bash.bashrc || cat << EOF >> /etc/bash.bashrc
 set input-meta on
 set output-meta on
 set convert-meta off
 EOF
 
+# clean up
+apt-get clean
 
 # Terraform Install
 [[ ! $(which terraform) ]] \
@@ -55,45 +52,40 @@ EOF
     && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
     && rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.*
 
-# Setting for ssh
-[[ ! $(cat /etc/ssh/ssh_config|grep \^StrictHostKeyChecking) ]] \
-    && echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+pip uninstall pyOpenSSL
+
+# # Setting for ssh
+# [[ ! $(cat /etc/ssh/ssh_config|grep \^StrictHostKeyChecking) ]] \
+#     && echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
 # Setting for Convenient
-echo "export EDITOR=vi" >> /etc/bash.bashrc
-
-## Alias for Terraform Apply
+grep -qxF "export EDITOR=vi" /etc/bash.bashrc || echo "export EDITOR=vi" >> /etc/bash.bashrc
+# Terraform Apply alias
 cmd='
 terraform destroy -auto-approve
 terraform init
 terraform apply -auto-approve
 cat terraform.tfstate|grep public_ip|grep -v associate
 '
-echo "alias ta=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
+grep -q "^alias ta=" /etc/bash.bashrc || echo "alias ta=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
 
-## Alias for Terraform Destroy
+# Terraform Destroy alias
 cmd='terraform destroy -auto-approve
 '
-echo "alias td=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
+grep -q "^alias td=" /etc/bash.bashrc || echo "alias td=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
 
 ## Alias for Delete aws Key pair
 cmd='aws ec2 delete-key-pair --key-name mykey
 '
-echo "alias dk=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
+# Check if alias already exists before adding
+grep -q "^alias dk=" /etc/bash.bashrc || echo "alias dk=\"echo '$cmd';$cmd\"" >> /etc/bash.bashrc
 
-
-
-cat <<EOF>> /etc/bash.bashrc
-    alias s1="ssh -t 'vm01' 'sudo su -c  bash'"
-    alias s2="ssh -t 'vm02' 'sudo su -c  bash'"
-    alias s3="ssh -t 'vm03' 'sudo su -c  bash'"
-EOF
+. ~/.bashrc
 
 
 
 
 
-# clean up
-apt-get clean
 
-. ~/.profile
+
+
--- Lab1.Kubespray/Lab3.InstanceForKubernetes/doSetHosts.sh
+++ Lab1.Kubespray/Lab3.InstanceForKubernetes/doSetHosts.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-cat /etc/hosts |grep -v vm0>~/.tmp
-sudo cp ~/.tmp /etc/hosts
+tmpfile=$(mktemp)
+grep -v vm0 /etc/hosts > "$tmpfile"
+sudo cp "$tmpfile" /etc/hosts
+rm -f "$tmpfile"
 
-ec2IdsAndName=$(aws ec2 describe-instances     --filters Name=instance-state-name,Values=running     --query 'Reservations[*].Instances[].[InstanceId, Tags[?Key==`Name`]]' --output text|sed -z  "s/\\nName[[:blank:]]/,/g")
-for i in $ec2IdsAndName; do
-	  ec2id=$(echo $i|awk 'BEGIN{FS=","}{printf $1}')
-	  ec2Name=$(echo $i|awk 'BEGIN{FS=","}{printf $2}')
-    ip=$(aws ec2 describe-instances --instance-ids $ec2id --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
-    echo  $ip  $ec2Name
-sudo bash -c "echo  $ip  $ec2Name  >> /etc/hosts"
+aws ec2 describe-instances \
+  --filters Name=instance-state-name,Values=running \
+  --query 'Reservations[*].Instances[*].[InstanceId, Tags[?Key==`Name`].Value | [0], PublicIpAddress]' \
+  --output text |
+while read -r ec2id ec2Name ip; do
+  echo "$ip $ec2Name"
+  sudo bash -c "echo $ip $ec2Name >> /etc/hosts"
 done
 
 echo "/etc/hosts------------"
