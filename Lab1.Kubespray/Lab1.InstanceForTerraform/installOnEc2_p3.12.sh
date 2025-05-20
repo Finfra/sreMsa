32d22cc6510913778f682b7e33f267f7d817103a
NamJungGu <nowage@gmail.com>
2025-05-20 21:38:01 +0900

    Update installOnEc2_p3.12.sh

--- Lab1.Kubespray/Lab1.InstanceForTerraform/installOnEc2_p3.12.sh
+++ Lab1.Kubespray/Lab1.InstanceForTerraform/installOnEc2_p3.12.sh
@@ -2,52 +2,47 @@
 
 # Version Setting
 TERRAFORM_VERSION="1.8.3"
-ANSIBE_VERSION="9.5.1"           # core 2.16.6
+ANSIBE_VERSION="9.5.1"  # core 2.16.6
 
 # System Variable Setting
 export LC_ALL=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
-echo "export LC_ALL=C.UTF-8">>/etc/bash.bashrc
-echo "export DEBIAN_FRONTEND=noninteractive">>/etc/bash.bashrc
+grep -qxF "export LC_ALL=C.UTF-8" /etc/bash.bashrc || echo "export LC_ALL=C.UTF-8" >> /etc/bash.bashrc
+grep -qxF "export DEBIAN_FRONTEND=noninteractive" /etc/bash.bashrc || echo "export DEBIAN_FRONTEND=noninteractive" >> /etc/bash.bashrc
 
 hostname i1
 hostname > /etc/hostname
 
+apt update
+apt -y install unzip curl jq git wget vim locales software-properties-common lsb-release
 
+locale-gen en_US.UTF-8
+update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
-apt -y update
-apt -y install docker.io docker-compose unzip mysql-client jq
-groupadd docker
-usermod -G docker ubuntu
+# install python3.12 + pip
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update
+apt install -y apt install python3-pip python3.12-venv python3.12-dev python3.12-distutils
 
-
-# install pip
-apt install -y python3-full
-apt install -y python3-pip
-pip3 install --break-system-packages numpy
-
-[[ -f /usr/bin/python ]]&&rm /usr/bin/python
+[[ -f /usr/bin/python ]] && rm /usr/bin/python
 ln -s /usr/bin/python3.12 /usr/bin/python
 
 
-
 # install awscli
-python3 -m pip install --break-system-packages awscli
-#python3 -m pip install  awsebcli
+python3.12 -m pip install --break-system-packages awscli
 complete -C aws_completer aws
 
 # install ansible
-pip3 install --break-system-packages netaddr jinja2
-pip3 install --break-system-packages ansible==$ANSIBE_VERSION
+pip install --break-system-packages netaddr jinja2
+pip install --break-system-packages ansible==$ANSIBE_VERSION
 
-# for Language Setting
-cat <<EOF>> /etc/bash.bashrc
+# Language Setting
+grep -qxF "set input-meta on" /etc/bash.bashrc || cat <<EOF >> /etc/bash.bashrc
 set input-meta on
 set output-meta on
 set convert-meta off
 EOF
-
 
 # Terraform Install
 [[ ! $(which terraform) ]] \
@@ -55,45 +50,39 @@ EOF
     && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
     && rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.*
 
-# Setting for ssh
-[[ ! $(cat /etc/ssh/ssh_config|grep \^StrictHostKeyChecking) ]] \
-    && echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+# 편의 설정
+grep -qxF "export EDITOR=vi" /etc/bash.bashrc || echo "export EDITOR=vi" >> /etc/bash.bashrc
 
-# Setting for Convenient
-echo "export EDITOR=vi" >> /etc/bash.bashrc
-
-## Alias for Terraform Apply
+# Terraform Apply alias
 cmd='
 terraform destroy -auto-approve
 terraform init
 terraform apply -auto-approve
 cat terraform.tfstate|grep public_ip|grep -v associate
 '
-echo "alias ta=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
+grep -q "^alias ta=" /etc/bash.bashrc || echo "alias ta=\"echo '$cmd';$cmd\"" >> /etc/bash.bashrc
 
-## Alias for Terraform Destroy
+# Terraform Destroy alias
 cmd='terraform destroy -auto-approve
 '
-echo "alias td=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
+grep -q "^alias td=" /etc/bash.bashrc || echo "alias td=\"echo '$cmd';$cmd\"" >> /etc/bash.bashrc
 
-## Alias for Delete aws Key pair
+# Delete AWS Key Pair
 cmd='aws ec2 delete-key-pair --key-name mykey
 '
-echo "alias dk=\"echo '$cmd';$cmd\"">>/etc/bash.bashrc
+grep -q "^alias dk=" /etc/bash.bashrc || echo "alias dk=\"echo '$cmd';$cmd\"" >> /etc/bash.bashrc
 
-
-
-cat <<EOF>> /etc/bash.bashrc
-    alias s1="ssh -t 'vm01' 'sudo su -c  bash'"
-    alias s2="ssh -t 'vm02' 'sudo su -c  bash'"
-    alias s3="ssh -t 'vm03' 'sudo su -c  bash'"
+# SSH alias 예시
+cat <<EOF >> /etc/bash.bashrc
+alias s1="ssh -t 'vm01' 'sudo su -c bash'"
+alias s2="ssh -t 'vm02' 'sudo su -c bash'"
+alias s3="ssh -t 'vm03' 'sudo su -c bash'"
 EOF
 
+# SSH StrictHostKeyChecking 끄기
+grep -q "^StrictHostKeyChecking" /etc/ssh/ssh_config || echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
-
-
-
-# clean up
+# Clean
 apt-get clean
 
-. ~/.profile
+. ~/.bashrc
